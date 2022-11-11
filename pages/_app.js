cbc39d9bf2224600c227eba1ee991e0aeacb85f3
ariessa-deriv <>
2022-11-11 09:51:20 +0800

    Add apollo client

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,40 @@
 import "../styles/globals.css";
 import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  HttpLink,
+} from "@apollo/client";
+
+const createApolloClient = () => {
+  const link = new HttpLink({
+    uri: "http://localhost:4000",
+  });
+
+  return new ApolloClient({
+    link,
+    cache: new InMemoryCache(),
+    // Enable sending cookies over cross-origin requests
+    credentials: "include",
+  });
+};
 
 const MyApp = ({ Component, pageProps }) => {
   return (
-    <GoogleReCaptchaProvider
-      reCaptchaKey={process.env.NEXT_PUBLIC_RECAPTHA_SITE_KEY}
-      scriptProps={{
-        async: false, // optional, default to false,
-        defer: true, // optional, default to false
-        appendTo: "body", // optional, default to "head", can be "head" or "body",
-        nonce: undefined,
-      }}
-    >
-      <Component {...pageProps} />;
-    </GoogleReCaptchaProvider>
+    <ApolloProvider client={createApolloClient()}>
+      <GoogleReCaptchaProvider
+        reCaptchaKey={process.env.NEXT_PUBLIC_RECAPTHA_SITE_KEY}
+        scriptProps={{
+          async: false, // optional, default to false,
+          defer: true, // optional, default to false
+          appendTo: "body", // optional, default to "head", can be "head" or "body",
+          nonce: undefined,
+        }}
+      >
+        <Component {...pageProps} />;
+      </GoogleReCaptchaProvider>
+    </ApolloProvider>
   );
 };
 
