dffa9461a9f50fce46b6518065373b0a4f2dd310
ariessa-deriv <>
2022-11-15 16:25:40 +0800

    Add profile and trades history page

---
+++ pages/profile.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Footer from "./components/Footer";
+import Head from "next/head";
+
+const Profile = () => {
+  return (
+    <>
+      <Head>
+        <title>Profile | Syntrade</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+    </>
+  );
+};
+
+export default Profile;
---
+++ pages/trades_history.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Footer from "./components/Footer";
+import Head from "next/head";
+
+const TradesHistory = () => {
+  return (
+    <>
+      <Head>
+        <title>Trades History | Syntrade</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+    </>
+  );
+};
+
+export default TradesHistory;
