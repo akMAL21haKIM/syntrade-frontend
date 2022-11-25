21c6a17043f4faef8a945b025595e23441ef129c
ariessa-deriv <>
2022-11-25 09:58:21 +0800

    Add flowbite package

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
@@ -19,4 +20,5 @@ module.exports = {
       },
     },
   },
+  plugins: [require("flowbite/plugin")],
 };
