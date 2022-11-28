d86800a9135c435311b92855f462b5ddde1bb18c
Isaiah Tan <108507316+IsaiahXingWei@users.noreply.github.com>
2022-11-28 09:48:28 +0800

    Tooltip function added

--- pages/components/SideMenu.jsx
+++ pages/components/SideMenu.jsx
@@ -10,12 +10,15 @@ import { classNames } from "../../lib/utilities";
 
 const Tooltip = ({ msg = " A left aligned tooltip.", children }) => {
   return (
-    <div class="relative flex justify-center align-center group w-full">
-      <div class="absolute flex flex-col z-99 top-0 items-center hidden group-hover:-top-[3rem] group-hover:flex">
-        <span class="flex z-10 p-2 -left-10 text-xs leading-none text-white bg-black shadow-lg">
+    <div class="relative flex justify-left align-left group w-full">
+      <div
+        class="absolute z-99 top-0 hidden group-hover:flex"
+        style={{ left: "-3rem" }}
+      >
+        <span class="flex z-10 p-2 text-xs leading-none text-white bg-black shadow-lg">
           {msg}
         </span>
-        <div class="absolute -bottom-[0.4rem] w-3 h-3 -ml-2 rotate-45 bg-black hover:-top-2"></div>
+        <div class="absolute bottom-[0.5rem] w-3 h-3 ml-[1.75rem] rotate-45 bg-black hover:-top-2"></div>
       </div>
       {children}
     </div>
