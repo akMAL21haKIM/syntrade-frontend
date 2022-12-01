44acdd4ee447650307d5cd500dc467cf60b55f43
ariessa-deriv <>
2022-12-01 20:07:10 +0800

    Fix syntax error in tooltip component

--- components/Tooltip.jsx
+++ components/Tooltip.jsx
@@ -2,18 +2,15 @@ export default function Tooltip({ msg, children, stakePayoutError }) {
   return (
     <div className="relative flex justify-left items-center align-left group w-full">
       <div
-        class={`absolute w-[13rem] z-99 ${
+        className={`absolute w-[13rem] z-99 ${
           stakePayoutError ? "flex" : "hidden"
         }`}
         style={{ left: "-14rem" }}
-        className={`absolute w-[6rem] z-99 ${
-          stakePayoutError ? "flex" : "hidden"
-        }`}
       >
         <span className="flex z-10 p-2 text-xs leading-none text-white bg-red-600 shadow-lg">
           {msg}
         </span>
-        <div class="absolute bottom-[1rem] w-3 h-3 ml-[12.5rem] rotate-45 bg-red-600 hover:-top-2"></div>
+        <div className="absolute bottom-[1rem] w-3 h-3 ml-[12.5rem] rotate-45 bg-red-600 hover:-top-2"></div>
         <div className="absolute bottom-[1rem] w-3 h-3 ml-[5.5rem] rotate-45 bg-red-600 hover:-top-2"></div>
       </div>
       {children}
