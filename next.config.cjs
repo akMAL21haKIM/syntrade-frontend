cd3eeb1432218b6577ab496ff958aa7aead99914
ariessa-deriv <>
2022-12-01 22:45:02 +0800

    Clean next.config.cjs file

--- next.config.cjs
+++ next.config.cjs
@@ -4,11 +4,4 @@ const nextConfig = {
   swcMinify: true,
 };
 
-// const withTM = require("next-transpile-modules")([
-//   "lightweight-charts",
-//   "fancy-canvas",
-// ]);
-
-// module.exports = withTM(nextConfig);
-
 module.exports = nextConfig;
