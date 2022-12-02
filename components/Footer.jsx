a35a574cefe69b6e3718cff84a6ac06407f83fdb
akMAL <akmal.hakim@besquare.com.my>
2022-12-02 09:11:58 +0000

    sligtly fixed report page responsiveness

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer>
-      <div className="absolute h-10 text-center p-2 bg-transparent text-gray-500 inset-x-0 bottom-0 text-sm">
+      <div className="fixed h-10 text-center p-2 bg-transparent text-gray-500 inset-x-0 bottom-0 text-sm">
         <p>© 2022 Syntrade. All rights reserved.</p>
       </div>
     </footer>
