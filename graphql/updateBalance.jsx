580b308d41e2d957add07fc45581ccd642f51767
ariessa-deriv <>
2022-12-02 20:50:33 +0800

    Remove updateBalance mutation

--- graphql/updateBalance.jsx
+++
@@ -1,9 +0,0 @@
-import { useQuery, useMutation, gql } from "@apollo/client";
-
-const UpdateBalance = gql`
-  mutation UpdateBalance($userId: Int!, $negatedParsedStakePayout: Float!) {
-    updateBalance(user_id: $userId, stakePayout: $negatedParsedStakePayout)
-  }
-`;
-
-export default UpdateBalance;
