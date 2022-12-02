74387c748fe5795c175d155d8c333759440f68ae
ariessa-deriv <>
2022-12-03 02:44:59 +0800

    Change variables naming convention to camelCase

--- graphql/createTrade.jsx
+++ graphql/createTrade.jsx
@@ -2,20 +2,20 @@ import { gql } from "@apollo/client";
 
 const CreateTrade = gql`
   mutation CreateTrade(
-    $user_id: Int!
-    $synthetic_type: String!
-    $option_type: String!
-    $wager_amount: Float!
+    $userId: Int!
+    $syntheticType: String!
+    $optionType: String!
+    $wagerAmount: Float!
     $ticks: Int!
-    $last_digit_prediction: Int
+    $lastDigit_Prediction: Int
   ) {
     createTrade(
-      user_id: $user_id
-      synthetic_type: $synthetic_type
-      option_type: $option_type
-      wager_amount: $wager_amount
+      userId: $userId
+      syntheticType: $syntheticType
+      optionType: $optionType
+      wagerAmount: $wagerAmount
       ticks: $ticks
-      last_digit_prediction: $last_digit_prediction
+      lastDigitPrediction: $lastDigitPrediction
     )
   }
 `;
