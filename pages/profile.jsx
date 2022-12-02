f821e23530dbf6cd877753918882f592bf5dd633
ariessa-deriv <>
2022-12-02 20:48:35 +0800

    Add popup modals to profile page

--- pages/profile.jsx
+++ pages/profile.jsx
@@ -4,9 +4,12 @@ import Head from "next/head";
 import Link from "next/link";
 import NavBar from "../components/NavBar";
 import { SkeletonLoaderProfilePage } from "../components/SkeletonLoaders";
+import Modal from "../components/Modal";
 
 const Profile = () => {
   const [loader, setLoader] = useState(false);
+  const [openChangePasswordModal, setOpenChangePasswordModal] = useState(false);
+  const [openDeleteAccountModal, setOpenDeleteAccountModal] = useState(false);
 
   useEffect(() => {
     setLoader(true);
@@ -28,74 +31,93 @@ const Profile = () => {
         {loader ? (
           <SkeletonLoaderProfilePage />
         ) : (
-          <div
-            id="profile_container"
-            className="flex justify-center items-center w-full mt-12"
-          >
-            <div id="profile_subcontainer" className="text-center">
-              <span
-                id="profile_avatar_space"
-                className="inline-block h-24 w-24 overflow-hidden rounded-full bg-gray-100"
-              >
-                <svg
-                  id="profile_avatar"
-                  className="h-full w-full text-gray-300"
-                  fill="currentColor"
-                  viewBox="0 0 24 24"
+          <>
+            <Modal
+              id="modal-change-password"
+              openModal={openChangePasswordModal}
+              setOpenModal={setOpenChangePasswordModal}
+              modalTitle="Change Password"
+              modalDescription="Change here"
+            />
+            <Modal
+              id="modal-delete-account"
+              openModal={openDeleteAccountModal}
+              setOpenModal={setOpenDeleteAccountModal}
+              modalTitle="Delete Account"
+              modalDescription="Are you sure you want to deactivate your account? All of
+                        your data will be permanently removed from our servers
+                        forever. This action cannot be undone."
+            />
+            <div
+              id="profile_container"
+              className="flex justify-center items-center w-full mt-12"
+            >
+              <div id="profile_subcontainer" className="text-center">
+                <span
+                  id="profile_avatar_space"
+                  className="inline-block h-24 w-24 overflow-hidden rounded-full bg-gray-100"
                 >
-                  <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
-                </svg>
-              </span>
-              <p className="text-base pt-4 font-semibold text-gray-500">
-                Joined on June 2022
-              </p>
+                  <svg
+                    id="profile_avatar"
+                    className="h-full w-full text-gray-300"
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
+                  </svg>
+                </span>
+                <p className="text-base pt-4 font-semibold text-gray-500">
+                  Joined on June 2022
+                </p>
 
-              <div id="email_container" className="space-y-2 pt-8 w-full">
-                <label
-                  htmlFor="email"
-                  className="block text-sm font-semibold text-gray-700 text-left"
-                >
-                  Your email
-                </label>
-                <div id="email_field" className="mt-1">
-                  <input
-                    id="email"
-                    name="email"
-                    type="email"
-                    autoComplete="email"
-                    // placeholder="user@example.com"
-                    required
-                    className="placeholder:normal-case select-none lowercase block w-full appearance-none rounded-md border-2 font-medium border-gray-200 px-3 py-2 text-gray-400 placeholder-gray-300 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                    disabled
-                    defaultValue="user@example.com"
-                  />
+                <div id="email_container" className="space-y-2 pt-8 w-full">
+                  <label
+                    htmlFor="email"
+                    className="block text-sm font-semibold text-gray-700 text-left"
+                  >
+                    Your email
+                  </label>
+                  <div id="email_field" className="mt-1">
+                    <input
+                      id="email"
+                      name="email"
+                      type="email"
+                      autoComplete="email"
+                      placeholder="yourname@domain.com"
+                      required
+                      className="placeholder:normal-case select-none lowercase block w-full appearance-none rounded-md border-2 font-medium border-gray-200 px-3 py-2 text-gray-400 placeholder-gray-300 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                      disabled
+                    />
+                  </div>
                 </div>
-              </div>
 
-              <div id="buttons_container" className="mt-8 flex justify-center">
                 <div
-                  id="change_password_field"
-                  className="inline-flex rounded-md shadow"
+                  id="buttons_container"
+                  className="mt-8 flex justify-center"
                 >
-                  <Link
-                    href="/reset_password"
-                    className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-7 py-3 text-base font-medium text-white hover:bg-indigo-700"
+                  <button
+                    id="button_change_password"
+                    className="inline-flex shadow rounded-md border border-transparent bg-indigo-600 px-7 py-3 hover:bg-indigo-700"
+                    onClick={(e) => setOpenChangePasswordModal(true)}
                   >
-                    Change password
-                  </Link>
-                </div>
+                    <p className="inline-flex items-center justify-center text-base font-medium text-white">
+                      Change password
+                    </p>
+                  </button>
 
-                <div id="delete_account_field" className="ml-3 inline-flex">
-                  <Link
-                    href="#"
-                    className="inline-flex items-center justify-center rounded-md border border-transparent bg-red-500 px-7 py-3 text-base font-medium text-white hover:bg-red-600"
+                  <button
+                    id="button_delete_account"
+                    className="ml-3 inline-flex rounded-md border border-transparent bg-red-500 px-7 py-3 hover:bg-red-600"
+                    onClick={(e) => setOpenDeleteAccountModal(true)}
                   >
-                    Delete account
-                  </Link>
+                    <p className="inline-flex items-center justify-center text-base font-medium text-white">
+                      Delete account
+                    </p>
+                  </button>
                 </div>
               </div>
             </div>
-          </div>
+          </>
         )}
       </main>
 
