4450bdce3b7bbcf7cba3f7f71dd9deb2a482cb59
ariessa-deriv <>
2022-12-02 21:04:23 +0800

    Update placeholders in inputs in login and signup pages to match reset password page

--- pages/login.jsx
+++ pages/login.jsx
@@ -173,7 +173,7 @@ const Login = () => {
                         name="email"
                         type="email"
                         autoComplete="email"
-                        placeholder="Enter email"
+                        placeholder="yourname@domain.com"
                         required
                         className={`placeholder:normal-case lowercase block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm ${
                           showEmailError ? "border-red-600" : ""
@@ -226,7 +226,7 @@ const Login = () => {
                         name="password"
                         type={openPassword === false ? "password" : "text"}
                         autoComplete="current-password"
-                        placeholder="Enter password"
+                        placeholder="●●●●●●●●"
                         required
                         className={`block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm ${
                           showPasswordError ? "border-red-600" : ""
--- pages/signup.jsx
+++ pages/signup.jsx
@@ -221,7 +221,7 @@ const SignUp = () => {
                         name="email"
                         type="email"
                         autoComplete="email"
-                        placeholder="Enter email"
+                        placeholder="yourname@domain.com"
                         required
                         className={`placeholder:normal-case lowercase block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm ${
                           showEmailError ? "border-red-600" : ""
@@ -272,7 +272,7 @@ const SignUp = () => {
                         name="password"
                         type={openPassword === false ? "password" : "text"}
                         autoComplete="current-password"
-                        placeholder="Enter password"
+                        placeholder="●●●●●●●●"
                         required
                         className={`block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm ${
                           showPasswordError ? "border-red-600" : ""
@@ -374,7 +374,7 @@ const SignUp = () => {
                           openConfirmPassword === false ? "password" : "text"
                         }
                         autoComplete="current-password"
-                        placeholder="Re-enter password"
+                        placeholder="●●●●●●●●"
                         required
                         className={`w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm ${
                           showConfirmPasswordError ? "border-red-600" : ""
